76706c00c3f51f73d788fec88b3c06446b1a3ebc
James Bradbury <jamesbradbury93@gmail.com>
2019-09-10 23:21:52 +0100

    check if valid file

--- ImportByFile.py
+++ ImportByFile.py
@@ -1,19 +1,23 @@
 from reaper_python import *
 import os
 
-
-#spine_folder = '/Users/jamesbradbury/dev/data_bending/groupings/spines/spines_short'
-#files = os.listdir(spine_folder)
+valid_files = [
+  '.wav',
+  '.aiff',
+  '.aif'
+]
 
 cancel, _, _, captions, user_input, two = RPR_GetUserInputs('Provide a folder name', 1, 'Folder Path:', '', 1024)
 
 if cancel != 0 and len(user_input) != 0:
   files = os.listdir(user_input)
   
+  
   for x in files:
-    RPR_InsertMedia(
-      os.path.join(user_input, x),
-      1
-    )
+    if os.path.splitext(x)[1] in valid_files:
+      RPR_InsertMedia(
+        os.path.join(user_input, x),
+        1
+      )
   
   
