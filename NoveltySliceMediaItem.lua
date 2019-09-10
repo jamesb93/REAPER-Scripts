72fbfbf8b2a5723ebdc42c149313dd30a4979d3a
James Bradbury <jamesbradbury93@gmail.com>
2019-09-10 23:50:38 +0100

    check if an item is selected

--- NoveltySliceMediaItem.lua
+++ NoveltySliceMediaItem.lua
@@ -34,43 +34,49 @@ end
 
 function sampstos(samples, sample_rate) return samples / sample_rate end
 
-local proj_path = reaper.GetProjectPathEx(0, "")
-local proj_name = reaper.GetProjectName(0, "")
-local cancel, user_inputs = reaper.GetUserInputs("Novelty Slice Parameters", 5, "feature,threshold,kernelsize,filtersize,fftsettings", "0,0.5,3,1,1024 512 1024")
+local num_selected_items = reaper.CountSelectedMediaItems(0)
+if cancel ~= false and num_selected_items > 0 then
+    local cancel, user_inputs = reaper.GetUserInputs("Novelty Slice Parameters", 5, "feature,threshold,kernelsize,filtersize,fftsettings", "0,0.5,3,1,1024 512 1024")
+    local item = reaper.GetSelectedMediaItem(0, 0)
+    local check_selected = reaper.IsMediaItemSelected(0)
+    reaper.ShowConsoleMsg(check_selected)
+    local proj_path = reaper.GetProjectPathEx(0, "")
+    local proj_name = reaper.GetProjectName(0, "")
 
-local params = commasplit(user_inputs)
-local feature = params[1]
-local threshold = params[2]
-local kernelsize = params[3]
-local filtersize = params[4]
-local fftsettings = params[5]
-  
-local temp_idx = proj_path .. "/fluid_novelty_slice_reaper.wav"
+    local params = commasplit(user_inputs)
+    local feature = params[1]
+    local threshold = params[2]
+    local kernelsize = params[3]
+    local filtersize = params[4]
+    local fftsettings = params[5]
+    
+    local temp_idx = proj_path .. "/fluid_novelty_slice_reaper.wav"
 
---   Each user MUST point this to their folder containing FluCoMa CLI executables
-local cli_path = '/Users/jamesbradbury/dev/bin'
---   Then we form some calls to the tools that will live in that folder
-local ie_exe = cli_path .. '/index_extractor '
-local ns_exe = cli_path .. '/noveltyslice '
+    --   Each user MUST point this to their folder containing FluCoMa CLI executables
+    local cli_path = '/Users/jamesbradbury/dev/bin'
+    --   Then we form some calls to the tools that will live in that folder
+    local ie_exe = cli_path .. '/index_extractor '
+    local ns_exe = cli_path .. '/noveltyslice '
 
---   Get info for item in REAPER
-local item = reaper.GetSelectedMediaItem(0, 0)
-local take = reaper.GetActiveTake(item)
-local src = reaper.GetMediaItemTake_Source(take)
-local sr = reaper.GetMediaSourceSampleRate(src)
-local full_path = reaper.GetMediaSourceFileName(src, '')
-local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-local ns_cmd = ns_exe .. " -source " .. full_path .. " -indices " .. temp_idx .. " -feature " .. feature .. " -kernelsize " .. kernelsize .. " -threshold " .. threshold .. " -filtersize " .. filtersize .. " -fftsettings " .. fftsettings
-local ie_cmd = ie_exe .. " " .. temp_idx
+    --   Get info for item in REAPER
+    local item = reaper.GetSelectedMediaItem(0, 0)
+    local take = reaper.GetActiveTake(item)
+    local src = reaper.GetMediaItemTake_Source(take)
+    local sr = reaper.GetMediaSourceSampleRate(src)
+    local full_path = reaper.GetMediaSourceFileName(src, '')
+    local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+    local ns_cmd = ns_exe .. " -source " .. full_path .. " -indices " .. temp_idx .. " -feature " .. feature .. " -kernelsize " .. kernelsize .. " -threshold " .. threshold .. " -filtersize " .. filtersize .. " -fftsettings " .. fftsettings
+    local ie_cmd = ie_exe .. " " .. temp_idx
 
-os.execute(ns_cmd)
-local slice_points_string = os.capture(ie_cmd, false)
-local slice_points = spacesplit(slice_points_string)
+    os.execute(ns_cmd)
+    local slice_points_string = os.capture(ie_cmd, false)
+    local slice_points = spacesplit(slice_points_string)
 
-for i=2, #slice_points do
-  local t_conversion = tonumber(slice_points[i])
-  local slice_pos = sampstos(t_conversion, sr)
-  item = reaper.SplitMediaItem(item, item_pos + slice_pos)
+    for i=2, #slice_points do
+    local t_conversion = tonumber(slice_points[i])
+    local slice_pos = sampstos(t_conversion, sr)
+    item = reaper.SplitMediaItem(item, item_pos + slice_pos)
+    end
+
+    reaper.UpdateArrange()
 end
-
-reaper.UpdateArrange()
