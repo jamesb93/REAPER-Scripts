b7005d062b008c16d68c2510e61f35f10f6c452e
James Bradbury <jamesbradbury93@gmail.com>
2019-09-11 17:19:53 +0100

    somewhat working verification

--- flucoma/FluidUtils.lua
+++ flucoma/FluidUtils.lua
@@ -46,13 +46,42 @@ end
 
 -- Functions pertaining to setting state --
 
-function set_fluid_path()
+function get_fluid_path()
+    return reaper.GetExtState("flucoma", "exepath")
+end
+
+function file_exists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
+function is_path_valid()
+    local fp = get_fluid_path()
+    local ns = fp .. "/noveltyslice"
+    if file_exists(ns) == true then
+        reaper.ShowMessageBox("The path you set looks good!", "Path Configuration", 0)
+    end
+    if file_exists(ns) == false then
+        reaper.ShowMessageBox("The path you set doesn't seem to contain the FluCoMa tools. Please try again.", "Path Configuration", 0)
+        path_setter()
+    end
+end
+
+function path_setter()
     local cancel, input = reaper.GetUserInputs("Set path to FluCoMa Executables", 1, "Path:, extrawidth=100", "/usr/local/bin")
     if cancel ~= false then
         local input_path = rm_trailing_slash(input)
         local sanitised_input_path = doublequote(input_path)
         reaper.SetExtState("flucoma", "exepath", sanitised_input_path, 1)
+        is_path_valid()
     end
+    if cancel == false then
+        reaper.ShowMessageBox("Your path remains unconfigured. The script will now exit.", "Warning", 0)
+    end
+end
+
+function set_fluid_path()
+    path_setter()
 end
 
 function check_state()
@@ -64,8 +93,4 @@ function sanity_check()
         reaper.ShowMessageBox("The path to the FluCoMa CLI tools is not set. Please follow the next prompt to configure it. Doing so remains persistent across projects and sessions of reaper. If you need to change it please use the FluidEditPath.lua script.", "Warning!", 0)
         set_fluid_path()
     end
-end
-
-function get_fluid_path()
-    return reaper.GetExtState("flucoma", "exepath")
 end
