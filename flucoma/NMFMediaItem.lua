f436251a6f8020c0354a5169ac85d261a686683e
James Bradbury <jamesbradbury93@gmail.com>
2019-09-12 12:54:36 +0100

    make things local

--- flucoma/NMFMediaItem.lua
+++ flucoma/NMFMediaItem.lua
@@ -23,35 +23,47 @@ if num_selected_items > 0 then
 
         local sr_t = {}
         local item_t = {}
-        nmf_cmd_t = {}
+        local nmf_cmd_t = {}
         local item_pos_t = {}
         local item_len_t = {}
         local full_path_t = {}
-        components_t = {}
+        local components_t = {}
         local item_pos_samples_t = {}
         local item_len_samples_t = {}
+        local take_ofs_t = {}
+        local take_ofs_samples_t = {}
 
         for i=1, num_selected_items do
 
             local item = reaper.GetSelectedMediaItem(0, i-1)
-            table.insert(item_t, item)
             local take = reaper.GetActiveTake(item)
             local src = reaper.GetMediaItemTake_Source(take)
             local sr = reaper.GetMediaSourceSampleRate(src)
-            table.insert(sr_t, sr)
             local full_path = reaper.GetMediaSourceFileName(src, '')
+            table.insert(item_t, item)
+            table.insert(sr_t, sr)
             table.insert(full_path_t, full_path)
-            table.insert(components_t, basename(full_path) .. "_c_" .. ".wav")
+            
 
+            local take_ofs = reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS")
             local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
             local item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+            table.insert(take_ofs_t, take_ofs)
             table.insert(item_pos_t, item_pos)
             table.insert(item_len_t, item_len)
+
+            -- Now make the name for the component.wav using the offset to create a unique id --
+            -- Using the offset means that slices won't share names at the output in the situation where you nmf on segments --
+            table.insert(components_t, basename(full_path) .. "_c_" .. tostring(take_ofs) .. ".wav")
+
+            local take_ofs_samples = stosamps(take_ofs, sr)
             local item_pos_samples = stosamps(item_pos, sr)
             local item_len_samples = stosamps(item_len, sr)
+            table.insert(take_ofs_samples_t, take_ofs_samples)
             table.insert(item_pos_samples_t, item_pos_samples)
             table.insert(item_len_samples_t, item_len_samples)
-            table.insert(nmf_cmd_t, nmf_exe .. " -source " .. doublequote(full_path) .. " -resynth " .. doublequote(components_t[i]) ..  " -fftsettings " .. fftsettings .. " -numframes " .. item_len_samples .. " -startframe " .. item_pos_samples .. " -components " .. components)
+
+            table.insert(nmf_cmd_t, nmf_exe .. " -source " .. doublequote(full_path) .. " -resynth " .. doublequote(components_t[i]) ..  " -fftsettings " .. fftsettings .. " -numframes " .. item_len_samples .. " -startframe " .. take_ofs_samples .. " -components " .. components)
         end
 
         -- Execute NMF Process
