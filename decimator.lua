de77e4361533f81d685617a6ef85346f9ab046f5
James Bradbury <jamesbradbury93@gmail.com>
2019-12-10 14:06:19 +0000

    deselect some percentage of currently selected items

---
+++ decimator.lua
@@ -0,0 +1,31 @@
+local info = debug.getinfo(1,'S');
+local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "_utils.lua")
+
+math.randomseed(os.clock() * 100000000000) -- random seed
+
+local num_selected_items = reaper.CountSelectedMediaItems(0)
+if num_selected_items > 0 then
+    local confirm, user_inputs = reaper.GetUserInputs("Decimator", 1, "percentage", "10")
+    if confirm then
+        reaper.Undo_BeginBlock()
+        -- Algorithm Parameters
+        local params = commasplit(user_inputs)
+        local percentage = tonumber(params[1])
+        local item_t = {}
+        
+        for i=1, num_selected_items do
+            local item = reaper.GetSelectedMediaItem(0, i-1)
+            table.insert(item_t, item)
+        end
+        
+        for i=1, num_selected_items do
+            local state = math.random() * 100.0 < percentage
+            reaper.SetMediaItemSelected(item_t[i], state)
+        end
+
+        reaper.UpdateArrange()
+        reaper.Undo_EndBlock("Decimator", 0)
+    end
+end
+::exit::
