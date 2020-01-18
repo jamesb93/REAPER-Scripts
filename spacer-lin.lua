80f5c74dbf638ae4be1e7b704cd24be81f5f3034
James Bradbury <jamesbradbury93@gmail.com>
2020-01-18 22:18:48 +0000

    linearly increasing spacer

--- spacer-lin.lua
+++ spacer-lin.lua
@@ -7,18 +7,33 @@ function commasplit(input_string)
     return t
 end
 
+function linspace(minimum, maximum, resolution) 
+    local range = maximum - minimum
+    local step_size = range / resolution
+    local t_linspace = {}
+    for i=1, resolution do
+        table.insert(t_linspace, i * step_size)
+    end
+    return t_linspace
+end
+
 math.randomseed(os.clock() * 100000000000) -- random seed
 
 local num_selected_items = reaper.CountSelectedMediaItems(0)
 if num_selected_items > 0 then
-    local confirm, user_inputs = reaper.GetUserInputs("Time Shift", 2, "min, max", "10, 50")
+    local captions = "min,max,noise factor (ms),exponent"
+    local caption_defaults = "10,50,0.0,1.0"
+    local confirm, user_inputs = reaper.GetUserInputs("Time Shift", 4, captions, caption_defaults)
     if confirm then
         reaper.Undo_BeginBlock()
         -- Algorithm Parameters
         local params = commasplit(user_inputs)
         local min = tonumber(params[1])
         local max = tonumber(params[2])
-
+        local noise_factor = tonumber(params[3])
+        local exponent = tonumber(params[4])
+        
+        local points = linspace(min, max, num_selected_items)
         local items_t = {}
 
         -- populate item table
@@ -28,21 +43,22 @@ if num_selected_items > 0 then
                 reaper.GetSelectedMediaItem(0, i-1)
             )
         end
-        
+    
         -- Now reverse iterate
-        while #items_t > 0 do
-            -- local random_offset = 0.3
-            local random_offset = ((math.random() * (max - min)) + min) / 1000
+        for i=1, #items_t do
+            
+            local offset = (points[i] / 1000)
             -- shift loop (each time move all items the same amount)
             for i=1, #items_t do
                 local item = items_t[i]
                 local pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-                
+                local noise = ((math.random()*2-1) * noise_factor) / 1000
+            
                 -- Set new position
                 reaper.SetMediaItemInfo_Value(
                     item,
                     "D_POSITION",
-                    pos + random_offset
+                    pos + offset + noise
                 )
             end
             -- Remove the left most item
