e9eca509f5b8b7e8363e7e6e7a11a2b92fd1a2f4
James Bradbury <jamesbradbury93@gmail.com>
2020-01-21 14:43:20 +0000

    show in finder script

---
+++ findershow.lua
@@ -0,0 +1,13 @@
+local num_selected_items = reaper.CountSelectedMediaItems(0)
+local operating_system = reaper.GetOS()
+
+if num_selected_items > 0 then
+    for i=1, num_selected_items do
+        local item = reaper.GetSelectedMediaItem(0, i-1)
+        local take = reaper.GetActiveTake(item)
+        local source = reaper.GetMediaItemTake_Source(take)
+        local path = reaper.GetMediaSourceFileName(source, "")
+        local cmd = "open -R " .. doublequote(path)
+        os.execute(cmd)
+    end
+end
