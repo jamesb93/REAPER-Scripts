2ca9c7f98473e3389a9c354f64f63f12ec289a3a
James Bradbury <jamesbradbury93@gmail.com>
2020-04-07 15:24:40 +0100

    render five tabs

---
+++ render_projects.lua
@@ -0,0 +1,7 @@
+reaper.Main_OnCommand(_SWS_FIRSTPROJTAB, 0)
+reaper.Main_OnCommand(42230, 0)
+
+reaper.Main_OnCommand(_SWS_PROJTAB2, 0)
+reaper.Main_OnCommand(_SWS_PROJTAB3, 0)
+reaper.Main_OnCommand(_SWS_PROJTAB4, 0)
+reaper.Main_OnCommand(_SWS_PROJTAB5, 0)
