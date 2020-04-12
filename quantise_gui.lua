1fd8a400e060362ae1d6d2a86a7ec9f31fd29c09
James Bradbury <jamesbradbury93@gmail.com>
2020-04-12 12:36:29 +0100

    work with scythe v3.0

--- quantise_gui.lua
+++ quantise_gui.lua
@@ -1,75 +1,65 @@
 local info = debug.getinfo(1,'S');
 local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
-dofile(script_path .. "/ReaCoMa/FluidPlumbing/FluidUtils.lua")
+dofile(script_path .. "utilities.lua")
 
-local lib_path = reaper.GetExtState("Lokasenna_GUI", "lib_path_v2")
-if not lib_path or lib_path == "" then
-    reaper.MB("Couldn't load the Lokasenna_GUI library. Please run 'Set Lokasenna_GUI v2 library path.lua' in the Lokasenna_GUI folder.", "Whoops!", 0)
+local libPath = reaper.GetExtState("Scythe v3", "libPath")
+if not libPath or libPath == "" then
+    reaper.MB("Couldn't load the Scythe library. Please install 'Scythe library v3' from ReaPack, then run 'Script: Scythe_Set v3 library path.lua' in your Action List.", "Whoops!", 0)
     return
 end
-loadfile(lib_path .. "Core.lua")()
+loadfile(libPath .. "scythe.lua")()
+local GUI = require("gui.core")
 
-GUI.req("Classes/Class - Slider.lua")()
-GUI.req("Classes/Class - Label.lua")()
-if missing_lib then return 0 end -- If any of the requested libraries weren't found, abort the script.
+local window = GUI.createWindow({
+    name = "Quantise contiguous items",
+    w = 500,
+    h = 100,
+  })
 
--- DEFINE INTERFACE --
-GUI.New("quantise_amt", "Slider", {
-    z = 1,
-    x = 10,
-    y = 20,
-    w = 400,
-    min = 1,
-    max = 2000,
-    defaults = 100,
-})
+local layer = GUI.createLayer({name = "Layer1"})
 
-GUI.New("quantise_noise", "Slider", {
-    z = 1,
-    x = 10,
-    y = 60,
-    w = 400,
-    min = 0,
-    max = 100,
-    defaults = 0,
-})
-
-GUI.New("quantise_amt_label", "Label", {
-    z = 2,
-    x = 420,
-    y = 20,
-    font = 3,
-    caption = "quantise",
-    shadow = 0
-})
-
-GUI.New("quantise_noise_label", "Label", {
-    z = 2,
-    x = 420,
-    y = 60,
-    font = 3,
-    caption = "noise",
-    shadow = 0
-})
-
-GUI.x, GUI.y, GUI.w, GUI.h = 200, 200, 500, 100
+layer:addElements(GUI.createElements(
+    {
+        name = "amount",
+        type = "Slider",
+        x = 10,
+        y = 30,
+        w = 400,
+        min = 1,
+        max = 2000,
+        defaults = 100,
+        caption = "Amount"
+    },
+    {
+        name = "noise",
+        type = "Slider",
+        x = 10,
+        y = 70,
+        w = 400,
+        min = 0,
+        max = 100,
+        defaults = 0,
+        caption = "Noise"
+    }
+))
 
 -- DEFINE LOGIC
-previous_spacing = GUI.Val("quantise_amt")
-previous_noise_factor   = GUI.Val("quantise_noise")
+previous_spacing = GUI.Val("amount")
+previous_noise_factor   = GUI.Val("noise")
+reaper.Undo_BeginBlock()
 
-function do_loop()
-    local spacing = GUI.Val("quantise_amt")
-    local noise_factor = GUI.Val("quantise_noise")
+local function Main()
+    local spacing = GUI.Val("amount")
+    local noise_factor = GUI.Val("noise")
     local num_selected_items = reaper.CountSelectedMediaItems(0)
     if num_selected_items > 0 and previous_noise_factor ~= noise_factor or previous_spacing ~= spacing then
         math.randomseed(os.clock() * 100000000000) -- random seed
 
         previous_spacing = spacing
         previous_noise_factor = noise_factor
-        reaper.Undo_BeginBlock()
 
-        local points = linspace(0, spacing * num_selected_items, num_selected_items)
+
+        local points = utils.linspace(0, spacing * num_selected_items, num_selected_items)
         local items_t = {}
         local first_item_pos = reaper.GetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0, 0), "D_POSITION")
         
@@ -86,8 +76,8 @@ function do_loop()
             
             local offset = (points[i] / 1000)
             -- shift loop (each time move all items the same amount)
-            for i=1, #items_t do
-                local item = items_t[i]
+            for j=1, #items_t do
+                local item = items_t[j]
                 local noise_offset = ((math.random()*2-1) * noise_factor) / 1000
                 -- Set new position
                 reaper.SetMediaItemInfo_Value(
@@ -104,14 +94,14 @@ function do_loop()
         end
         
         reaper.UpdateArrange()
-        reaper.Undo_EndBlock("Quantiser", 0)
+        
     end
 end
+reaper.Undo_EndBlock("Quantiser", 0)
+  
+window:addLayers(layer)
+window:open()
 
-GUI.version = 0
-GUI.func = do_loop
-GUI.freq = 0
-
-
-GUI.Init()
+GUI.func = Main
+GUI.funcTime = 0
 GUI.Main()
